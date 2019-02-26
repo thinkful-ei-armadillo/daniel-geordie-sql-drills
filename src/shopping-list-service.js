ec0d79b07c94aaf22d43e608b26dc888615f374d
Daniel Ors <do7811@gmail.com>
2019-02-26 15:52:47 -0500

    completed main calls and promises

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -4,17 +4,17 @@ const shoppingService = {
   getAll(knex) {
     return knex.select('*').from('shopping_list');
   },
-  insertItem(knex, contents) {
-    return knex.insert(contents).into('shopping_list').returning('*').then(rows => rows[0]);
-  },
   getById(knex, id) {
     return knex.from('shopping_list').select('*').where('id', id).first();
   },
   deleteItems(knex, id) {
-    return knex('shopping_list').where({id}).delete();
+    return knex('shopping_list').where({ id }).delete();
   },
-  updateItem(knex, id, data) {
-    return knex('shopping_list').where({id}).update(data);
+  updateItem(knex, id, newItem) {
+    return knex('shopping_list').where({ id }).update(newItem);
+  },
+  insertItem(knex, contents) {
+    return knex.insert(contents).into('shopping_list').returning('*').then(rows => rows[0]);
   },
 };
 
--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,5 +1,4 @@
 'use strict';
-
 require('dotenv').config();
 const knex = require('knex');
 const shoppingService = require('./shopping-list-service');
@@ -12,6 +11,25 @@ const knexInstance = knex({
 console.log('connection successful');
 
 shoppingService.getAll(knexInstance)
-  .then(result => {
-    console.log(result);
+  .then(result => {console.log(result);})
+  .then(() => 
+    shoppingService.insertItem(knexInstance, {
+      name: 'new name',
+      price: 1,
+      date_added: new Date(),
+      checked: false,
+      category: 'Main'
+    })
+  )
+  .then(newItem => {
+    console.log(newItem);
+    return shoppingService.updateItem(
+      knexInstance,
+      newItem.id,
+      { name: 'updated name'}
+    ).then(() => shoppingService.getById(knexInstance, newItem.id));
+  })
+  .then(item => {
+    console.log(item);
+    return shoppingService.deleteItems(knexInstance, item.id);
   });
